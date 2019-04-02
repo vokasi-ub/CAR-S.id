c3a58a00451f4820560286cdd3845dd7ed427288
Sonifebi <soniafebe98@gmail.com>
2019-04-02 23:34:10 +0700

    template

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\category;
+use Illuminate\Support\Facades\DB;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return view('dashboard.index', compact('categories'));
+    }
+}
